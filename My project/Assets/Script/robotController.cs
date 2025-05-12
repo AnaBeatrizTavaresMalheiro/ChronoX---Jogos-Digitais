7a203ffc21ad0f58350fa3f39162259c874a4225
ANA BEATRIZ DE SOUZA <unifasouza@feinet.fei.edu.br>
2025-05-12 20:35:56 -0300

    Adicao fundo/amim

---
+++ My project/Assets/Script/robotController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robotController : MonoBehaviour
+{
+
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float move = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+        }
+        
+    }
+}
